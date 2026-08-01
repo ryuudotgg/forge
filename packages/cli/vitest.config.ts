f90e7a03bf1bb6402d575bc62a3e274ffbc2287c
Ryuu <me@ryuu.gg>
2026-08-01 04:46:48 +0400

    test(cli): disable colors in vitest env

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -5,12 +5,13 @@ const { perFileOverrides, ...thresholds } = PACKAGES["@ryuujs/forge"].temper;
 
 export default defineConfig({
 	test: {
+		environment: "node",
+		env: { NO_COLOR: "1" },
+		include: ["tests/**/*.test.ts"],
 		coverage: {
 			include: ["src/**"],
 			reporter: ["text", "json-summary", "json"],
 			thresholds: { ...thresholds, ...perFileOverrides },
 		},
-		environment: "node",
-		include: ["tests/**/*.test.ts"],
 	},
 });
--- packages/generators/vitest.config.ts
+++ packages/generators/vitest.config.ts
@@ -6,12 +6,12 @@ const { perFileOverrides, ...thresholds } =
 
 export default defineConfig({
 	test: {
+		environment: "node",
+		include: ["tests/**/*.test.ts"],
 		coverage: {
 			include: ["src/**"],
 			reporter: ["text", "json-summary", "json"],
 			thresholds: { ...thresholds, ...perFileOverrides },
 		},
-		environment: "node",
-		include: ["tests/**/*.test.ts"],
 	},
 });
